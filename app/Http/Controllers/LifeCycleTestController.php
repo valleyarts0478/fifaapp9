e2cad9d89a8e6ab5f70449175d731644b012f3a2
valleyarts0478 <arts047855@yahoo.co.jp>
2021-07-15 19:46:07 +0900

    サービスプロバイダのテスト

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class LifeCycleTestController extends Controller
 {
+    public function showserviceProviderTest(){
+        
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample, $password, $encrypt->decrypt($password));
+
+    }
     public function showserviceContainerTest()
     {
         app()->bind('lifeCycleTest', function(){
