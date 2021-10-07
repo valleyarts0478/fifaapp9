e92635d63ab7a2a7df36302e2c59b1fec358eefa
valleyarts0478 <arts047855@yahoo.co.jp>
2021-10-07 21:25:51 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -5,16 +5,23 @@ namespace App\Services;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 
-Class ImageService
+class ImageService
 {
- public static function upload($imageFile, $folderName)
- {
-    $fileName = uniqid(rand().'_');
-    $extension = $imageFile->extension();
-    $fileNameToStore = $fileName. '.' . $extension;
-    $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
-    Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
-    
-    return $fileNameToStore;
- }
+   public static function upload($imageFile, $folderName)
+   {
+      // dd($imageFile['image']);
+      if (is_array($imageFile)) {
+         $file = $imageFile['image'];
+      } else {
+         $file = $imageFile;
+      }
+
+      $fileName = uniqid(rand() . '_');
+      $extension = $file->extension();
+      $fileNameToStore = $fileName . '.' . $extension;
+      $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
+      Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
+
+      return $fileNameToStore;
+   }
 }
