40a65169faa67651ce7ef98ea553369b5fae6688
valleyarts0478 <arts047855@yahoo.co.jp>
2021-10-17 13:20:21 +0900

    product create

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -13,12 +13,13 @@
                   <x-auth-validation-errors class="mb-4" :errors="$errors" />  
                   <form method="post" action="{{ route('owner.shops.update', ['shop' => $shop->id ]) }}" enctype="multipart/form-data">
                         @csrf
-                         <div class="p-2 w-1/2 mx-auto">
+                      <div class="-m-2">
+                           <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                   <label for="name" class="leading-7 text-sm text-gray-600">店名 ※必須</label>
                                   <input type="text" id="name" name="name"  value="{{ $shop->name }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
-                              </div>
+                           </div>
                               <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                   <label for="information" class="leading-7 text-sm text-gray-600">店舗情報 ※必須</label>
@@ -38,7 +39,7 @@
                               <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                               <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
-                          </div>
+                         </div> --}}
                           <div class="p-2 w-1/2 mx-auto">
                             <div class="relative flex justify-around">
                               <div><input type="radio" name="is_selling" value="1" class="mr-2" @if($shop->is_selling === 1){ checked } @endif >販売中</div>
@@ -50,6 +51,7 @@
                             <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                           </div>
                         </div>
+                      </div>
                     </form>
                 </div>
             </div>
