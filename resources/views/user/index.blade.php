4cefecc1eefba9c806418bdc7b549a6d5b967565
valleyarts0478 <arts047855@yahoo.co.jp>
2021-10-24 13:47:04 +0900

    show調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -12,7 +12,7 @@
                     <div class="flex flex-wrap">
                             @foreach($products as $product)
                         <div class="w-1/4 p-4">
-                        {{-- <a href="{{ route('owner.products.edit', ['product' => $product->id]) }}"> --}}
+                        <a href="{{ route('user.items.show', ['item' => $product->id]) }}">
                             <div class="border rounded-md p-4">
                                 <x-thumbnail filename="{{ $product->filename ?? ''}}" type="products" />
                                     <div class="mt-4">
