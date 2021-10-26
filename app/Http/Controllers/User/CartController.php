6e7f9af882b28d88fc38d0f07aae86150d7bc482
valleyarts0478 <arts047855@yahoo.co.jp>
2021-10-26 21:53:07 +0900

    決済後のカート内商品削除

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -88,7 +88,7 @@ class CartController extends Controller
             'payment_method_types' => ['card'],
             'line_items' => [$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index'),
+            'success_url' => route('user.cart.success'),
             'cancel_url' => route('user.cart.index'),
         ]);
 
@@ -98,5 +98,11 @@ class CartController extends Controller
             'user.checkout',
             compact('session', 'publicKey')
         );
+    }
+    public function success()
+    {
+        Cart::where('user_id', Auth::id())->delete();
+
+        return redirect()->route('user.items.index');
     }
 }
