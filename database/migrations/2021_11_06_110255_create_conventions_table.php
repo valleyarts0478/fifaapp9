5b880c8cb05e20da41a2ff3f9c5940c65ffa00d1
valleyarts0478 <arts047855@yahoo.co.jp>
2021-11-06 12:15:08 +0900

    index表示まで確認済み

---
+++ database/migrations/2021_11_06_110255_create_conventions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConventionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conventions', function (Blueprint $table) {
+            $table->id();
+            $table->string('convention_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conventions');
+    }
+}
--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -39,7 +39,7 @@
                               <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                               <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
-                         </div> --}}
+                         </div>
                           <div class="p-2 w-1/2 mx-auto">
                             <div class="relative flex justify-around">
                               <div><input type="radio" name="is_selling" value="1" class="mr-2" @if($shop->is_selling === 1){ checked } @endif >販売中</div>
