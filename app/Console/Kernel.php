3d13f86366c29670103ccff999817c498b1da426
valleyarts0478 <arts047855@yahoo.co.jp>
2022-03-12 22:57:19 +0900

    team_owner/login18

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command:Batchtest')->everyMinute();;
+        // $schedule->command('conventionbatch:test')->everyMinute();
+        $schedule->command('conventionbatch:test');
     }
 
     /**
