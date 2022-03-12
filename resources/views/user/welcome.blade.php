78ce30071d93add550951fd99456e00e38682605
valleyarts0478 <arts047855@yahoo.co.jp>
2022-03-12 11:35:49 +0900

    fix1a

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -1,11 +1,4 @@
 <x-front.app>
-    testしています。testしています。testしています。
-    {{-- <x-slot name="header">ヘッダー</x-slot>
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    testしています。
 
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-           <a href="{{ route('team_owner.login') }}" class="text-sm text-gray-700 underline">Log in</a>
-        </div>
-    </div>
-    <x-slot name="footer">フッター</x-slot> --}}
 </x-front.app>
