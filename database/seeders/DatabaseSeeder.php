a36584eae205309fb6828d5edc46bbd71f43a40c
valleyarts0478 <arts047855@yahoo.co.jp>
2022-03-21 23:18:07 +0900

    section_add prod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             AdminSeeder::class,
-            Ownerseeder::class,
+            // Ownerseeder::class,
             // Shopseeder::class,
             // ImageSeeder::class,
             // CategorySeeder::class,
