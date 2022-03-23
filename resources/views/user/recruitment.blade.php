f973a510cb932f3d46a14987ebdc958cda6d34d8
valleyarts0478 <arts047855@yahoo.co.jp>
2022-03-23 16:09:08 +0900

    recruitment完成

--- resources/views/user/recruitment.blade.php
+++ resources/views/user/recruitment.blade.php
@@ -1,8 +1,44 @@
 <x-front.app>
 
+    <div class="max-w-screen-lg py-4 mx-auto">
+        <div class="containermd:px-8">
+            <div class=""><img src="{{ asset('storage/top/' . "recruitment_bn.jpg") }}"></div>
+            <h2 class="bg-ral-400 text-white text-2xl p-4">参加チーム募集！</h2>
+         <div class="px-4 leading-loose tracking-wide">
+            <p>RAL-Eではリーグ戦にご参加いただけるチームを募集しています。</p>
+            <p>参加条件はRAL-Eが定めるレギュレーションを順守していただけるチームのみです。</p>
+            <p>2022年3月時点でPS4での開催となっています。</p>
+            <h3 class="my-8 font-bold border-b border-ral-400">RAL-Eはこんなチームに向いています。</h3>
+                <ul class="mx-4 list-disc">
+                    <li>少人数しかいないチーム（5人～）</li>
+                    <li>楽しみながらリーグ戦をしたい</li>
+                    <li>とにかくプロクラブが大好き</li>
+                </ul>
 
+               <h3 class="my-8 font-bold border-b border-ral-400">RALとRAL-Eについて</h3> 
+               <p>RALとRAL-Eは協力関係にあります。</p>
+               <p>他では味わえない合同カップ戦も開催され毎シーズン盛り上がりをみせています。</p>
+               
+               <h3 class="my-8 font-bold border-b border-ral-400">お問い合わせ</h3>
+               <p>参加をしたい、まずは聞きたいことがあるなどお気軽にお問い合わせください。</p>
+               <div class="my-8 text-center">ご連絡はTwitterからどうぞ</div>
 
+                <!-- social - start -->
+                <div class="w-full mx-auto">
+                    <div class="flex items-center justify-center">
+                    <a href="#" target="_blank" class="text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100">
+                        <svg class="w-5 h-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                        </svg>
+                    </a>                   
+                    <div class="ml-2"><a href="https://twitter.com/TEDs886" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@TEDs886</a></div>
+                    </div>
+                </div>
+                <!-- social - end -->
 
+         </div>
+        </div><!--container END-->
+    </div>
 
 
 
