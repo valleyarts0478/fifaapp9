8606d53a54ed0ff5c99f3304e9f6a970b9510032
valleyarts0478 <arts047855@yahoo.co.jp>
2022-04-28 22:56:13 +0900

    文言修正

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -7,6 +7,7 @@
         <span class="text-ral-400 text-xl font-bold">RAL-E</span>
       </a>
       <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2022 RAL-E
+        <a href="https://twitter.com/ral_efl" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@ral_efl</a>
         <a href="https://twitter.com/RALE_TEDs" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@RALE_TEDs</a>
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
--- resources/views/user/recruitment.blade.php
+++ resources/views/user/recruitment.blade.php
@@ -21,7 +21,8 @@
                
                <h3 class="my-8 font-bold border-b border-ral-400">お問い合わせ</h3>
                <p>参加をしたい、まずは聞きたいことがあるなどお気軽にお問い合わせください。</p>
-               <div class="my-8 text-center">ご連絡はTwitterからどうぞ</div>
+               <p>運営者は2名です。どちらにご連絡いただいても構いません。</p>
+               <div class="my-8 text-center">ご連絡はお気軽にTwitterからどうぞ</div>
 
                 <!-- social - start -->
                 <div class="w-full mx-auto">
@@ -30,7 +31,8 @@
                         <svg class="w-5 h-5 -ml-4" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
                         </svg>
-                    </a>                   
+                    </a>
+                    <div class="ml-2"><a href="https://twitter.com/ral_efl" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@ral_efl</a></div>
                     <div class="ml-2"><a href="https://twitter.com/RALE_TEDs" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@RALE_TEDs</a></div>
                     </div>
                 </div>
