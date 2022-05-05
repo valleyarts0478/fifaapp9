c3a56d2d7d3fc83b1ffbdbd54fb0b1eacb698ccd
valleyarts0478 <arts047855@yahoo.co.jp>
2022-05-05 22:45:07 +0900

    ツイート用ボタン設置

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -11,17 +11,12 @@
         <a href="https://twitter.com/RALE_TEDs" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@RALE_TEDs</a>
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-        <a href="https://twitter.com/RALE_TEDs" class="ml-3 text-gray-500" target="_blank">
-          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-          </svg>
-        </a>
-        {{-- <a class="ml-3 text-gray-500">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-          </svg>
-        </a> --}}
-      </span>
+        <span class="text-sm mr-2">ツイート用</span>
+      <a href="https://twitter.com/intent/tweet?hashtags=RAL-E,FIFA22,プロクラブ" class="twitter-hashtags-btn" target="_blank">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+    </span>
     </div>
   </footer>
