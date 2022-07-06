c535589c0c401e18f809e1e0e4bcc01a65b96f1a
valleyarts0478 <arts047855@yahoo.co.jp>
2022-07-06 21:14:04 +0900

    修正

--- resources/views/admin/game_check/index.blade.php
+++ resources/views/admin/game_check/index.blade.php
@@ -28,7 +28,7 @@
                         <td class="border px-4 py-2">
                             @if($data->game_result->home_goal === NULL or  $data->game_result->away_goal === NULL)
                             空欄あり
-                            @elseif(($data->game_result->home_goal + $data->game_result->away_goal) === $data->total_goal)
+                            @elseif(($data->game_result->home_goal + $data->game_result->away_goal) == $data->total_goal)
                             {{-- @elseif(($data->game_result->home_goal + $data->game_result->away_goal + $data->game_result->home_own_goal + $data->game_result->away__own_goal) == $data->total_goal) --}}
                             OK
                             @else
