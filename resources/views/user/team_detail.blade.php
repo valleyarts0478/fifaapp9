2e753ee16a9d28ab66e132a3bcff9380758b70a1
valleyarts0478 <arts047855@yahoo.co.jp>
2022-10-29 10:21:11 +0900

    player_card修正1

--- resources/views/user/team_detail.blade.php
+++ resources/views/user/team_detail.blade.php
@@ -13,13 +13,13 @@
           <div class="p-2 w-1/2 md:w-1/3">
             {{-- <x-player_icon :player=$player></x-player_icon> --}}
               <div class="w-full h-64 pt-8 md:pt-16 md:h-96 bg-contain bg-no-repeat bg-center" style="background-image: url({{ asset('storage/players/' . 'player_card1.png')}})">
-                  <div class="ml-5 lg:ml-16 text-ral-400 text-4xl md:text-5xl">
+                  <div class="ml-5 lg:ml-16 text-white text-4xl md:text-5xl">
                     {{ $player->position->position_name }}
                   </div>
-                  <div class="w-8 lg:w-12 text-ral-400 text-3xl text-center md:text-5xl mt-1 ml-5 lg:ml-16">{{ $player->player_no }}</div>
-                  <div class="text-center text-xl md:text-3xl text-gray-900 title-font font-medium mt-8 md:mt-16">{{ $player->player_name }}</div>
-                  <div class="w-8 h-8 md:w-12 md:h-12 mx-auto mt-2">
-                    <img src="{{ asset('storage/teams/logo/' . $player->team_owner->team_logo_url) }}">
+                  <div class="w-8 lg:w-12 text-white text-3xl text-center md:text-5xl mt-1 ml-5 lg:ml-16">{{ $player->player_no }}</div>
+                  <div class="text-center text-xl md:text-3xl text-white title-font font-medium mt-8 md:mt-16">{{ $player->player_name }}</div>
+                  <div class="w-full text-center text-white mx-auto mt-2">
+                    {{ $player->team_owner->team_abb }}
                   </div>
               </div>
           </div>
