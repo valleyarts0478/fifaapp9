52b96fa167ce96f34b7956c5a73fc793623ea881
valleyarts0478 <arts047855@yahoo.co.jp>
2022-11-29 10:14:16 +0900

    ツイッタータグ修正

--- resources/views/components/front/footer.blade.php
+++ resources/views/components/front/footer.blade.php
@@ -12,7 +12,7 @@
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
         <span class="text-sm mr-2">ツイート用</span>
-      <a href="https://twitter.com/intent/tweet?hashtags=RALE,FIFA22,プロクラブ" class="text-ral-400 twitter-hashtags-btn" target="_blank">
+      <a href="https://twitter.com/intent/tweet?hashtags=RALE,FIFA23,プロクラブ" class="text-ral-400 twitter-hashtags-btn" target="_blank">
         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
         </svg>
