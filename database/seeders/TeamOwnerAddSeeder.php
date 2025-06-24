dac9f89cf27a30ba07b23c4214b7b5d7560fee05
valleyarts0478 <arts047855@yahoo.co.jp>
2025-06-24 20:48:40 +0900

    20250624-2

--- database/seeders/TeamOwnerAddSeeder.php
+++ database/seeders/TeamOwnerAddSeeder.php
@@ -17,45 +17,84 @@ class TeamOwnerAddSeeder extends Seeder
     {
         DB::table('team_owners')->insert([
 
-    [
-        'id'=> '296',
-        'name'=> 'SEA BREEZE',
-        'email'=> 'sea@rale.net15',
-        'password'=> Hash::make("sea191"),
-        'convention_id'=> '15',
-        'league_id'=> '1',
-        'team_name'=> 'SEA BREEZE',
-        'team_abb'=> 'SEA',
-        'twitter_add'=> '@sea_proclub',
-        'team_logo_url'=> 'SEA.jpg',
-        'created_at'=> '2025-02-25 00:00:00'
-    ],
-   [
-        'id'=> '297',
-        'name'=> 'ReVeL EDGE',
-        'email'=> 'rvl@rale.net15',
-        'password'=> Hash::make("rvl281"),
-        'convention_id'=> '15',
-        'league_id'=> '2',
-        'team_name'=> 'ReVeL EDGE',
-        'team_abb'=> 'RVL',
-        'twitter_add'=> '@ReVeL_EDGE',
-        'team_logo_url'=> 'RVL.jpg',
-        'created_at'=> '2025-02-25 00:00:00'
-    ],
-   [
-        'id'=> '298',
-        'name'=> 'Joga Bonito FCB',
-        'email'=> 'jbf@rale.net15',
-        'password'=> Hash::make("jbf201"),
-        'convention_id'=> '15',
-        'league_id'=> '1',
-        'team_name'=> 'Joga Bonito FCB',
-        'team_abb'=> 'JBF',
-        'twitter_add'=> '@jogabonitofc25',
-        'team_logo_url'=> 'JBF.jpg',
-        'created_at'=> '2025-02-25 00:00:00'
-    ],
+            [
+                'id'=> '299',
+                'name'=> 'SwitchTheRipper',
+                'email'=> 'str@rale.net16',
+                'password'=> Hash::make("str29"),
+                'convention_id'=> '16',
+                'league_id'=> '2',
+                'team_name'=> 'SwitchTheRipper',
+                'team_abb'=> 'STR',
+                'twitter_add'=> '@switchtheripper',
+                'team_logo_url'=> 'STR.png',
+                'created_at'=> '2025-06-25 00:00:00'
+            ],
+        [
+                'id'=> '300',
+                'name'=> 'Spirit Bomb',
+                'email'=> 'spb@rale.net16',
+                'password'=> Hash::make("spb619"),
+                'convention_id'=> '16',
+                'league_id'=> '1',
+                'team_name'=> 'Spirit Bomb',
+                'team_abb'=> 'SPB',
+                'twitter_add'=> '@BombSpirit2025',
+                'team_logo_url'=> 'SPB.png',
+                'created_at'=> '2025-06-25 00:00:00'
+            ],
+        [
+                'id'=> '301',
+                'name'=> 'ALMOND CHOCO',
+                'email'=> 'uma@rale.net16',
+                'password'=> Hash::make("uma311"),
+                'convention_id'=> '16',
+                'league_id'=> '1',
+                'team_name'=> 'ALMOND CHOCO',
+                'team_abb'=> 'UMA',
+                'twitter_add'=> '@ALMOND_CHOCO_11',
+                'team_logo_url'=> 'UMA.png',
+                'created_at'=> '2025-06-25 00:00:00'
+            ],
+        [
+                'id'=> '302',
+                'name'=> 'Gimme Shelter',
+                'email'=> 'gsr@rale.net16',
+                'password'=> Hash::make("gsr917"),
+                'convention_id'=> '16',
+                'league_id'=> '1',
+                'team_name'=> 'Gimme Shelter',
+                'team_abb'=> 'GSR',
+                'twitter_add'=> '@G_S2025',
+                'team_logo_url'=> 'GSR.png',
+                'created_at'=> '2025-06-25 00:00:00'
+            ],
+        [
+                'id'=> '303',
+                'name'=> 'Another Sky',
+                'email'=> 'ans@rale.net16',
+                'password'=> Hash::make("ans411"),
+                'convention_id'=> '16',
+                'league_id'=> '2',
+                'team_name'=> 'Another Sky',
+                'team_abb'=> 'ANS',
+                'twitter_add'=> '@FIFA2056917045',
+                'team_logo_url'=> 'ANS.png',
+                'created_at'=> '2025-06-25 00:00:00'
+            ],
+        [
+                'id'=> '304',
+                'name'=> 'Mokey Turn',
+                'email'=> 'mtv@rale.net16',
+                'password'=> Hash::make("mtv23"),
+                'convention_id'=> '16',
+                'league_id'=> '2',
+                'team_name'=> 'Mokey Turn',
+                'team_abb'=> 'MTV',
+                'twitter_add'=> '@monkeyturn_2004',
+                'team_logo_url'=> 'MTV.png',
+                'created_at'=> '2025-06-25 00:00:00'
+            ],
      ]);
     }
 }
--- resources/views/team_owner/players/index.blade.php
+++ resources/views/team_owner/players/index.blade.php
@@ -56,7 +56,7 @@
                                     class="text-white bg-ral-400 border-0 py-2 px-4 mr-1 focus:outline-none rounded-md text-sm">編集</button>
                             </div> --}}
                             <!-- 削除ボタン -->
-                            {{-- <form id="delete_{{ $player->id }}" method="post"
+                            <form id="delete_{{ $player->id }}" method="post"
                                 action="{{ route('team_owner.players.destroy', ['player' => $player->id]) }}">
                                 @csrf
                                 @method('delete')
@@ -64,7 +64,7 @@
                                     <a href="#" data-id="{{ $player->id }}" onclick="deletePost(this)"
                                         class="text-white bg-ral-200 border-0 py-2 px-4 mr-1 focus:outline-none rounded-md text-sm">削除</a>
                                 </td>
-                            </form> --}}
+                            </form>
                             <!-- 削除ボタン END-->
                         </div>
                     </div>
